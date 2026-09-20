27d65706ee2b6fbd47083e28fb7b7aa8e08a4a68
IyongOfficial <aceeria3@gmail.com>
2026-09-20 13:44:54 +0800

    Update UniversalLoader.lua

--- UniversalLoader.lua
+++ UniversalLoader.lua
@@ -1,1 +1,7 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/aceeria3-lab/IOHUBUniversal/refs/heads/main/RideAPet.lua"))()
+loadstring(game:HttpGet("local placeId = game.PlaceId
+
+if placeId == 124216119978534 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/aceeria3-lab/IOHUBUniversal/refs/heads/main/RideAPet.lua"))()
+else
+    game.Players.LocalPlayer:Kick("Game is Not Supported")
+end"))()
