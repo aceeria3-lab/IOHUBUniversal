4a50d46d394d29a8adca256b7d488bd8e34ed913
IyongOfficial <aceeria3@gmail.com>
2026-09-20 14:01:46 +0800

    Update UniversalLoader.lua

--- UniversalLoader.lua
+++ UniversalLoader.lua
@@ -1,17 +1,9 @@
 local placeId = game.PlaceId
 
-if placeId == 124216119978534 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/aceeria3-lab/IOHUBUniversal/refs/heads/main/RideAPet.lua"))()
+if placeId == 107778070777162 then
+    -- Unsupported game
+    game.Players.LocalPlayer:Kick("Game is Not Supported Ask Iyong Official")
 else
-    -- Force official Roblox error/kick screen with Leave button
-    local Players = game:GetService("Players")
-    local LocalPlayer = Players.LocalPlayer
-    
-    pcall(function()
-        LocalPlayer:Kick("Game is Not Supported")
-    end)
-    
-    -- Backup method (kung hindi pa rin lumabas yung Kick)
-    task.wait(0.5)
-    game:GetService("GuiService"):SetError("Game is Not Supported Ask Iyong Official")
+    -- Supported (lahat ng ibang game)
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/aceeria3-lab/IOHUBUniversal/refs/heads/main/RideAPet.lua"))()
 end
